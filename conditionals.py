838cc9b3ff0ca3662481037a549b5deaa2563d12
Osagie Okoedo <okoedooc@yahoo.com>
2021-09-19 06:50:42 +0100

    use of input and if-else condition

---
+++ conditionals.py
@@ -0,0 +1,7 @@
+userReply = input("Do you need to ship a package? (Type yes or no)")
+
+if userReply == "yes":
+    print("We can help you ship that package!")
+else:
+    print("Please come back when you do need to ship a package. Thank you.")
+
